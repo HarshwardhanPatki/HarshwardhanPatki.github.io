b788606df7e8e73e5de994961dab759eccf6ff5a
HarshwardhanPatki <95173792+HarshwardhanPatki@users.noreply.github.com>
2023-01-02 20:18:00 +0530

    My Portfolio Site

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ===================================== Typing Animation ============================================= */
+var typed = new Typed(".typing",{
+    strings:["","UnderGraduate","Problem Solver","Student at Pimpri Chinchwad College Of Engineering (PCCOE)"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
